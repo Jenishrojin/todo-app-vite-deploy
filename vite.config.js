348061f89071c712b38b6199a232d27eeaa0dfb0
Jenishrojin <jenishrojin2002@gmail.com>
2024-09-09 19:26:52 +0530

    initial upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "todo-app-vite-deploy/",
   plugins: [react()],
-  base: "todo-app-vite-deploy" 
+  
 })
